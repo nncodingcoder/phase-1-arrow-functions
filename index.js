82619670fbe68fcbd0fe2bea46cafc2ad24814e3
naomiinyele <inyeleinyele@gmail.com>
2023-09-20 01:23:19 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function () {
+    return 2000 / 100
+}
+
+const square = (length) => {
+    return length * length
+}
+square(2)
+
+
+const add = (parameter1, parameter2) => parameter1 + parameter2
